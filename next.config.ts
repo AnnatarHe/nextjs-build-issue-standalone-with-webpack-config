a9bb36adb1bdf2c1814582868da6b62177971c87
AnnatarHe <annatar.he+github@gmail.com>
2024-11-30 01:14:16 +0000

    not build well after made some changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   /* config options here */
+  reactStrictMode: true,
+  output: 'standalone',
+  webpack: (config) => config
 };
 
 export default nextConfig;
